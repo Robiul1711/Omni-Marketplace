1fa7435157b234ac55fe523c656a27e56a2db903
Setulal Chowhan <setulalchowhan@gmail.com>
2026-04-18 10:19:33 +0600

    feat: add Home page layout and workspace configuration file

--- src/pages/sites/Home.jsx
+++ src/pages/sites/Home.jsx
@@ -9,7 +9,7 @@ import ReadyStarted from "@/components/sites/home/ReadyStarted";
 
 const Home = () => {
   return (
-    <div className="text-black! flex flex-col xlg:gap-[120px] md:gap-14 gap-8">
+    <div className="text-black! flex lg:flex-col flex-col xlg:gap-[120px] md:gap-14 gap-8">
       <HeroBanner />
       <Explore />
       <MarketPlaceworks />
